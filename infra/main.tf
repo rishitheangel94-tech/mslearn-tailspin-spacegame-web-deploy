cbe2bba71247befe5545b1a31318ec6492d67284
rishitheangel94-tech <rishitheangel94@gmail.com>
2026-03-01 15:54:30 -0500

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "${var.app_name}-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v3"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    always_on = true
+  }
+}
